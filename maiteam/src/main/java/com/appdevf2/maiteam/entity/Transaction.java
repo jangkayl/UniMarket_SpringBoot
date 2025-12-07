fa10884b2ba8a5f2d8553e6f1ac67e0f43c06ee4
John Kyle Villarazo <villarazojk@gmail.com>
2025-12-07 13:42:54 +0800

    fixed getter setter in transaction

--- maiteam/src/main/java/com/appdevf2/maiteam/entity/Transaction.java
+++ maiteam/src/main/java/com/appdevf2/maiteam/entity/Transaction.java
@@ -34,4 +34,85 @@ public class Transaction {
     @ManyToOne
     @JoinColumn(name = "item_id", referencedColumnName = "itemId")
     private Item item;
+
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Student getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(Student buyer) {
+        this.buyer = buyer;
+    }
+
+    public Student getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Student seller) {
+        this.seller = seller;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
 }
